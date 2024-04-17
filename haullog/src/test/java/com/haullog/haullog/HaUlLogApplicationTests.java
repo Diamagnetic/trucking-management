8d998c7477ae2a94a1c09afbd11ce71830632931
Chirag Dhamange <c.dhamange@gmail.com>
2024-04-17 12:39:30 -0700

    Backend initial setup with springboot

---
+++ haullog/src/test/java/com/haullog/haullog/HaUlLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haullog.haullog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaUlLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
